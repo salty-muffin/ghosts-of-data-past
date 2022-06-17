8b9f14d933b0187b775fafdd88e3fdbabfe52151
papayapeter <t.babel@gmx.com>
2022-06-17 17:43:50 +0200

    improved visual output of the generate script

--- generate/generate.py
+++ generate/generate.py
@@ -9,6 +9,8 @@ import click
 import shortuuid
 from io import BytesIO
 from walrus import Database
+import climage
+from termcolor import colored
 
 from generators.image_generator import ImageGenerator
 from generators.text_generator import TextGenerator
@@ -21,11 +23,11 @@ SENDER_STRING = '[{}:]'  # how is the sender to be formatted
 
 
 # click parsers
-def parse_roles(s: Union[str, List]) -> List[str]:
+def parse_comma_list(s: Union[str, List]) -> List[str]:
     if isinstance(s, list):
         return s
 
-    return [role for role in map(str.strip, str(s).split(','))]
+    return [item for item in map(str.strip, str(s).split(','))]
 
 
 def parse_random_factor(s: Union[str, List]) -> List[float]:
@@ -40,7 +42,8 @@ def parse_random_factor(s: Union[str, List]) -> List[float]:
 @click.option('--gptdir',       type=click.Path(exists=True),   help='directory of gpt2 model', required=True)
 @click.option('--stylegandir',  type=click.Path(exists=True),   help='directory of stylegan3 model file (formatted like this: \'folder/{{role}}_stylegan3_model.pkl\')', required=True)
 @click.option('--prompt',       type=str,                       help='starting prompt', required=True)
-@click.option('--roles',        type=parse_roles,               help='list of roles (e.g \'artist, scientist\')', required=True)
+@click.option('--roles',        type=parse_comma_list,          help='list of roles (e.g \'artist, scientist\')', required=True)
+@click.option('--colors',       type=parse_comma_list,          help='colors for the roles in the terminal (should be the same count as roles)', required=True)
 @click.option('--basetime',     type=float,                     default=3.0, help='minimum time for writing all types of messages', required=True)
 @click.option('--lettertime',   type=float,                     default=0.2, help='time it takes to write one letter', required=True)
 @click.option('--imagetime',    type=float,                     default=6.0, help='time it takes to take an image', required=True)
@@ -52,6 +55,7 @@ def generate(
         stylegandir: str,
         prompt: str,
         roles: List[str],
+        colors: List[str],
         basetime: float,
         lettertime: float,
         imagetime: float,
@@ -77,7 +81,9 @@ def generate(
         writing_state[role].update(writer=role, state=0)
 
     # get all notification sound paths in a list
-    sound_paths = glob.glob(os.path.join('generate', 'sounds', '*'))
+    sound_paths = glob.glob(
+        os.path.join('generate', 'notification-sounds', '*')
+        )
 
     # set variables
     image_seed = {}
@@ -92,7 +98,8 @@ def generate(
         while True:
             # get the sender and set them to writing
             previous_sender = sender
-            sender = roles[random.randint(0, len(roles) - 1)]
+            role_index = random.randint(0, len(roles) - 1)
+            sender = roles[role_index]
             sender_readfactor = 0.2 if previous_sender == sender else 1
 
             # wait for the read duration
@@ -119,6 +126,7 @@ def generate(
             # TODO optimize: use the whole response
 
             # only go on, if there are matches, else repeat
+            image_string = ''
             if matches:
                 # get message
                 text = response[:matches[0].start()].strip()
@@ -141,6 +149,8 @@ def generate(
                         progressive=True
                         )
                     image_data = image_output.getvalue()
+                    image_string = climage.convert(image_output, width=40)
+
                     image_output.close()
 
                     alt = f'selfie of {sender}'
@@ -163,9 +173,10 @@ def generate(
                     sound_data = file.read()
 
                 # relaod all sound effects if all of them have been used
-                sound_paths = glob.glob(
-                    os.path.join('generate', 'sounds', '*')
-                    )
+                if (not sound_paths):
+                    sound_paths = glob.glob(
+                        os.path.join('generate', 'notification-sounds', '*')
+                        )
 
                 # wait if message generation was shorter than minimum write_duration
                 min_duration = basetime + len(text) * lettertime
@@ -191,7 +202,8 @@ def generate(
                 message.expire(120)
                 writing_state[sender].update(writer=sender, state=0)
 
-                print(sender.upper(), text, bool(image_data))
+                print(f'{sender}>', colored(text, colors[role_index]))
+                if (image_string): print(image_string, end='')
 
     except KeyboardInterrupt:
         raise SystemExit
