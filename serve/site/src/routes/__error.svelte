ecb7f35f94c09c925572666bc08b03ba7fef28ec
papayapeter <t.babel@gmx.com>
2022-07-01 10:28:45 +0200

    added custom error page

---
+++ serve/site/src/routes/__error.svelte
@@ -0,0 +1,29 @@
+<script context="module" lang="ts">
+	import type { Load } from '@sveltejs/kit';
+
+	export const load: Load = async ({ error, status }) => ({
+		props: { message: error?.message, status }
+	});
+</script>
+
+<script lang="ts">
+	export let message: string;
+	export let status: number;
+</script>
+
+<div class="error__wrapper">
+	<div class="error">
+		<h1>{status}</h1>
+		<p>{message}</p>
+	</div>
+</div>
+
+<style lang="scss">
+	.error__wrapper {
+		height: 100vh;
+
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+</style>
