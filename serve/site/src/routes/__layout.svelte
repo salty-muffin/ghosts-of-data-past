bb5cade6d7da01cfd81741d1d5afc383aa6f056e
papayapeter <t.babel@gmx.com>
2022-09-05 10:41:01 +0200

    only recieveing message after writing

--- serve/site/src/routes/__layout.svelte
+++ serve/site/src/routes/__layout.svelte
@@ -28,35 +28,38 @@
 
 	// on incoming chat message from server
 	socket.on('chat_message', (message) => {
-		// process image data and if there, create a blob from it
-		let imageURL = '';
-		if (message.imageData.byteLength > 0) {
-			const arrayBufferView = new Uint8Array(message.imageData);
-			const blob = new Blob([arrayBufferView], { type: 'image/jpeg' });
+		// take care that message is only processed, if sender has been writing before
+		if ($writing.filter((element) => element.writer === message.sender).length > 0) {
+			// process image data and if there, create a blob from it
+			let imageURL = '';
+			if (message.imageData.byteLength > 0) {
+				const arrayBufferView = new Uint8Array(message.imageData);
+				const blob = new Blob([arrayBufferView], { type: 'image/jpeg' });
 
-			imageURL = URL.createObjectURL(blob);
-		}
-		// add new message to messages store
-		messages.add({
-			id: message.id,
-			sender: message.sender,
-			text: message.text,
-			imageURL: imageURL,
-			alt: message.alt,
-			timestamp: message.timestamp
-		});
-		writing.add({ writer: message.sender, state: false });
+				imageURL = URL.createObjectURL(blob);
+			}
+			// add new message to messages store
+			messages.add({
+				id: message.id,
+				sender: message.sender,
+				text: message.text,
+				imageURL: imageURL,
+				alt: message.alt,
+				timestamp: message.timestamp
+			});
+			writing.add({ writer: message.sender, state: false });
 
-		// play audio on new message (only if unmuted)
-		if (!$muted && $sound) {
-			// create a blob from sound data
-			const arrayBufferView = new Uint8Array(message.soundData);
-			const blob = new Blob([arrayBufferView], { type: 'audio/mpeg' });
+			// play audio on new message (only if unmuted)
+			if (!$muted && $sound) {
+				// create a blob from sound data
+				const arrayBufferView = new Uint8Array(message.soundData);
+				const blob = new Blob([arrayBufferView], { type: 'audio/mpeg' });
 
-			const soundURL = URL.createObjectURL(blob);
+				const soundURL = URL.createObjectURL(blob);
 
-			if ($sound) $sound.src = soundURL;
-			$sound.play();
+				if ($sound) $sound.src = soundURL;
+				$sound.play();
+			}
 		}
 	});
 
