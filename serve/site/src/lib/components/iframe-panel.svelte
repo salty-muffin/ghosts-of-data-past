8935a4b04d26ab09f444443d3ca40018ce6fb67a
papayapeter <t.babel@gmx.com>
2022-09-12 16:38:42 +0200

    finished video

--- serve/site/src/lib/components/iframe-panel.svelte
+++ serve/site/src/lib/components/iframe-panel.svelte
@@ -66,6 +66,15 @@
 </script>
 
 <div class="iframe-panel__wrapper {className}" bind:this={wrapper}>
+	<div class="iframe-panel__animation-wrapper" style="opacity: {playing ? '0' : '1'}">
+		<div class="iframe-panel__animation">
+			<div />
+			<span>x</span>
+			<div />
+			<span>x</span>
+			<div />
+		</div>
+	</div>
 	<iframe
 		class="iframe-panel__iframe"
 		style="width: {targetWidth}px; height: {targetHeight}px; opacity: {playing ? '1' : '0'}"
@@ -77,6 +86,8 @@
 </div>
 
 <style global lang="scss">
+	@use '../scss/variables' as *;
+
 	.iframe-panel__wrapper {
 		display: flex;
 		align-items: center;
@@ -89,5 +100,93 @@
 		pointer-events: none;
 		flex-shrink: 0;
 		transition: opacity 2s ease-in-out;
+	}
+
+	@keyframes fadeInOut {
+		0% {
+			opacity: 0;
+		}
+		30% {
+			opacity: 0;
+			transform: translateY(80%);
+		}
+		100% {
+			opacity: 1;
+			transform: translateY(-20%);
+		}
+	}
+
+	.iframe-panel__animation-wrapper {
+		display: inline-block;
+		position: absolute;
+		left: 50%;
+		right: 50%;
+		transform: translate(-50%, -50%);
+		width: 80px;
+		height: 80px;
+
+		z-index: -1;
+
+		transition: opacity 0.5s ease-in-out;
+	}
+
+	.iframe-panel__animation {
+		display: flex;
+		align-items: center;
+
+		div {
+			width: 1em;
+			height: 1em;
+
+			background-color: map-get($colors, 'foreground');
+			border-radius: 100%;
+
+			animation-name: fadeInOut;
+			animation-duration: 1.6s;
+			animation-timing-function: ease;
+			animation-iteration-count: infinite;
+			animation-direction: alternate;
+			animation-fill-mode: both;
+		}
+		div:nth-of-type(1) {
+			animation-delay: 0s;
+		}
+		div:nth-of-type(2) {
+			animation-delay: 0.2s;
+		}
+		div:nth-of-type(3) {
+			animation-delay: 0.4s;
+		}
+
+		span {
+			visibility: hidden;
+			width: map-get($margin-secondary, 'sm');
+		}
+	}
+
+	@media only screen and (min-width: map-get($breakpoint, 'md')) {
+		.iframe-panel__animation {
+			div {
+				width: 1.1em;
+				height: 1.1em;
+			}
+
+			span {
+				width: map-get($margin-secondary, 'md');
+			}
+		}
+	}
+
+	@media only screen and (min-width: map-get($breakpoint, 'lg')) {
+		.iframe-panel__animation {
+			div {
+				width: 1.3;
+				height: 1.3;
+			}
+
+			span {
+				width: map-get($margin-secondary, 'lg');
+			}
+		}
 	}
 </style>
--- serve/site/src/routes/video.svelte
+++ serve/site/src/routes/video.svelte
@@ -7,13 +7,24 @@
 	<IframePanel class="video__panel" url="https://player.vimeo.com/video/636103889" />
 	<Nav class="video__nav" links={[{ href: '/background', text: 'background' }]} />
 </div>
-<div class="video__text">
+<a href="#text">
+	<div class="video__arrow">
+		<svg width="30" height="37" viewBox="0 0 30 37" fill="none" xmlns="http://www.w3.org/2000/svg">
+			<path
+				d="M15 2V34M15 34L27 22.0784M15 34L3 22.0784"
+				stroke-width="3"
+				stroke-linecap="square"
+			/>
+		</svg>
+	</div>
+</a>
+<div class="video__text" id="text">
 	<p>
 		This is a video documentation of <em>ghosts of data past</em>.
 	</p>
 	<p>
-		The abstract and all other pages are visbile exactly like this in the artwork. Only this page
-		would usually hold the live generated chat of which a short excerpt is here documented.
+		The background texts and all other pages are visible exactly like this in the artwork. Only this
+		page would usually hold the live generated chat of which a short excerpt is here documented.
 	</p>
 </div>
 
@@ -25,6 +36,23 @@
 		width: 100vw;
 		height: 100vh;
 		max-height: -webkit-fill-available;
+	}
+
+	.video__arrow {
+		position: absolute;
+		bottom: map-get($nav-size, 'sm');
+		left: 0;
+
+		padding: 0.5em 0.5em 0.2em;
+		margin: 1em;
+
+		background-color: map-get($colors, 'background');
+
+		svg {
+			path {
+				stroke: map-get($colors, 'foreground');
+			}
+		}
 	}
 
 	.video__panel {
@@ -42,22 +70,41 @@
 	}
 
 	.video__text {
-		margin: map-get($margin-primary, 'sm');
-	}
+		padding: map-get($margin-primary, 'sm');
+		border-top: map-get($border-width, 'sm') solid map-get($colors, 'foreground');
 
-	@media only screen and (min-width: map-get($breakpoint, 'md')) {
-		.video__text {
-			margin: map-get($margin-primary, 'md');
+		& > *:first-of-type {
+			margin-top: 0;
+		}
+		& > *:last-of-type {
+			margin-bottom: 0;
 		}
 	}
 
-	@media only screen and (min-width: map-get($breakpoint, 'lg')) {
+	@media only screen and (min-width: map-get($breakpoint, 'md')) {
+		.video__arrow {
+			bottom: map-get($nav-size, 'lg');
+
+			svg {
+				width: 3em;
+				height: auto;
+			}
+
+			padding: 0.7em 0.7em 0.3em;
+			margin: 1.5em;
+		}
+
 		.video__text {
-			margin: map-get($margin-primary, 'lg');
+			padding: map-get($margin-primary, 'md');
+			border-top: map-get($border-width, 'md') solid map-get($colors, 'foreground');
 		}
 	}
 
 	@media only screen and (min-width: 80em) {
+		.video__arrow {
+			display: none;
+		}
+
 		.video__elements {
 			position: absolute;
 			bottom: 0;
@@ -67,22 +114,25 @@
 
 		.video__text {
 			position: absolute;
-			bottom: map-get($nav-size, 'lg');
+			bottom: map-get($nav-size, 'md');
 			left: 0;
 
 			margin: 2em;
 			padding: 2em;
 
-			width: 22em;
+			width: 19em;
+
+			border-top: none;
 
 			background-color: map-get($colors, 'background');
+		}
+	}
 
-			& > *:first-of-type {
-				margin-top: 0;
-			}
-			& > *:last-of-type {
-				margin-bottom: 0;
-			}
+	@media only screen and (min-width: map-get($breakpoint, 'lg')) {
+		.video__text {
+			bottom: map-get($nav-size, 'lg');
+
+			width: 22em;
 		}
 	}
 </style>
