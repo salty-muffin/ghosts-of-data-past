c5c512116d90df5a028cb8d2df9bc14deec8da21
salty-muffin <t.babel@gmx.com>
2023-02-01 00:17:16 +0100

    corrected typo

--- serve/site/src/routes/(doc)/source/+page.svelte
+++ serve/site/src/routes/(doc)/source/+page.svelte
@@ -7,9 +7,9 @@
 </div>
 <p>
 	In case you want to know how all this was implemented exactly or even recreate it with your own
-	datasets, you can the source code for this project and further documentation through the following
-	links. All source code used for research and production of this work is publicly available and may
-	be used, adapted and redistributed for non-commercial projects.
+	datasets, you can find the source code for this project and further documentation through the
+	following links. All source code used for research and production of this work is publicly
+	available and may be used, adapted and redistributed for non-commercial projects.
 </p>
 
 <ul>
