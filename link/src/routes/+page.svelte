81401f539204b66d1e10f282e6d28773582104cb
salty-muffin <t.babel@gmx.com>
2024-01-24 14:35:07 +0100

    implemented player from binary

--- link/src/routes/+page.svelte
+++ link/src/routes/+page.svelte
@@ -8,24 +8,11 @@
 
 	import { onMount } from 'svelte';
 	import Sketch from './scene';
-	import { timestamp } from '$lib/stores/timestamp';
-
-	const MAXRECORDING = 60;
 
 	let container: HTMLDivElement;
 	let canvas: HTMLCanvasElement;
 
-	let show = false;
-	let download = false;
-	let url = '';
-
 	let sketch: Sketch;
-
-	let relaxation: number;
-	let radius: number;
-	let strength: number;
-
-	$: if (sketch) sketch.setParameters(relaxation / 10000 + 0.99, radius / 100, strength / 100);
 
 	$: onMount(() => {
 		// load image
@@ -78,36 +65,8 @@
 				draw();
 				window.addEventListener('resize', draw);
 
-				sketch = new Sketch(container, canvas, MAXRECORDING);
+				sketch = new Sketch(container, canvas, 'data/distortion_data.dat');
 				sketch.animate();
-
-				relaxation = (sketch.relaxation - 0.99) * 10000;
-				radius = sketch.radius * 100;
-				strength = sketch.strength * 100;
-
-				window.addEventListener('keydown', (e) => {
-					if (e.key == ' ') {
-						if (!$timestamp.recording) {
-							sketch.startRecording();
-						} else {
-							sketch.stopRecording();
-						}
-					}
-					if (e.key == 'p') {
-						if (!sketch.playing) {
-							sketch.play();
-						} else {
-							sketch.stop();
-						}
-					}
-					if (e.key == 's') {
-						show = !show;
-					}
-					if (e.key == 'r') {
-						sketch.resetRecording();
-						download = false;
-					}
-				});
 			};
 			if (image.complete) {
 				init();
@@ -116,67 +75,10 @@
 			}
 		}
 	});
-
-	const saveData = () => {
-		if (sketch) {
-			const blob = new Blob([sketch.getDisplacementBuffer()]);
-
-			url = window.URL.createObjectURL(blob);
-
-			download = true;
-		}
-	};
-
-	const downloadData = () => {
-		const a = document.createElement('a');
-		a.href = url;
-		a.download = 'distortion_data.dat';
-		a.target = '_blank';
-		a.style.display = 'none';
-		document.body.appendChild(a);
-		a.click();
-		a.remove();
-
-		setTimeout(() => window.URL.revokeObjectURL(url), 20000);
-	};
 </script>
 
 <div class="link" bind:this={container}>
 	<canvas id="canvas" bind:this={canvas} />
-	<span id="timestamp" class="ui" class:recording={$timestamp.recording}>{$timestamp.time}</span>
-	<span id="info" class="ui">
-		press SPACE for recording, P for playback, S to display extras & R to reset recording
-	</span>
-	<div class="ui" id="save" class:show>
-		<button on:click={saveData}>save recording</button>
-		{#if download}
-			<button id="download" on:click={downloadData}>download recording</button>
-		{/if}
-	</div>
-	<div id="settings" class="ui" class:show>
-		<input
-			type="range"
-			id="relaxation"
-			name="relaxation"
-			min="0"
-			max="100"
-			step="5"
-			bind:value={relaxation}
-		/>
-		<label for="relaxation">relaxation</label>
-		<input type="range" id="radius" name="radius" min="0" max="100" step="5" bind:value={radius} />
-		<label for="radius">radius</label>
-		<input
-			type="range"
-			id="strength"
-			name="strength"
-			min="0"
-			max="100"
-			step="5"
-			bind:value={strength}
-		/>
-		<label for="strength">strength</label>
-	</div>
 </div>
 
 <style global lang="scss">
@@ -191,58 +93,5 @@
 		display: none;
 		pointer-events: none;
 		position: absolute;
-	}
-
-	.ui {
-		position: absolute;
-
-		color: white;
-	}
-
-	#timestamp {
-		left: 1em;
-		top: 1em;
-
-		&.recording {
-			color: red;
-		}
-	}
-
-	#save {
-		position: absolute;
-		left: 1em;
-		bottom: 1em;
-
-		color: black;
-
-		display: none;
-		&.show {
-			display: block;
-		}
-	}
-
-	#settings {
-		right: 1em;
-		bottom: 1em;
-
-		flex-direction: column;
-
-		display: none;
-		&.show {
-			display: flex;
-		}
-	}
-
-	#info {
-		position: absolute;
-		right: 1em;
-		top: 1em;
-
-		color: white;
-	}
-
-	.qr {
-		max-width: 70vh;
-		height: auto;
 	}
 </style>
--- link/src/routes/scene.ts
+++ link/src/routes/scene.ts
@@ -2,38 +2,25 @@ import * as THREE from 'three';
 import fragment from '$lib/shaders/fragment.glsl?raw';
 import vertex from '$lib/shaders/vertex.glsl?raw';
 
-import { timestamp } from '$lib/stores/timestamp';
-import { get } from 'svelte/store';
-
-const clamp = (n: number, min: number, max: number) => {
-	return Math.max(min, Math.min(n, max));
-};
-
 export default class Sketch {
 	container: HTMLDivElement;
 	canvas: HTMLCanvasElement;
+
+	dataURL: string;
 
 	scene: THREE.Scene;
 	camera: THREE.OrthographicCamera;
 	renderer: THREE.Renderer;
 
+	interval: number;
 	clock: THREE.Clock;
 	delta = 0;
-	interval = 1 / 60;
-
-	elapsedTime: THREE.Clock;
-	maxRecordingTime: number;
-	playing = false;
 
 	plane?: THREE.Mesh;
 	geometry?: THREE.PlaneGeometry;
 
-	gridDivider = 16;
 	gridX = 0;
 	gridY = 0;
-	relaxation = 0.995;
-	radius = 0.3;
-	strength = 0.1;
 
 	mouseDown = false;
 
@@ -45,22 +32,19 @@ export default class Sketch {
 	displacementTextureBuffer = new Float32Array([]);
 	frameSize = 0;
 	frameIndex = 0;
+	frameCount = 0;
 
-	mouse = {
-		x: 0,
-		y: 0,
-		vX: 0,
-		vY: 0,
-		prevX: 0,
-		prevY: 0
-	};
-
-	constructor(container: HTMLDivElement, canvas: HTMLCanvasElement, recordingLength = 10) {
+	constructor(
+		container: HTMLDivElement,
+		canvas: HTMLCanvasElement,
+		dataURL: string,
+		framerate = 60
+	) {
 		// store bindings
 		this.container = container;
 		this.canvas = canvas;
-
-		this.maxRecordingTime = recordingLength;
+		this.dataURL = dataURL;
+		this.interval = 1 / framerate;
 
 		// set up camera
 		this.scene = new THREE.Scene();
@@ -75,189 +59,68 @@ export default class Sketch {
 		// set up time keeping
 		this.clock = new THREE.Clock();
 
-		this.elapsedTime = new THREE.Clock(false);
+		// load the displacemt data
+		fetch(this.dataURL).then((response) => {
+			response.arrayBuffer().then((data) => {
+				// buffer for the dispacement data
+				this.displacementTextureBuffer = new Float32Array(data);
 
-		// adjust to viewport size
-		this.resize();
+				this.gridX = this.displacementTextureBuffer[0];
+				this.gridY = this.displacementTextureBuffer[1];
 
-		// set up contents
-		this.regenerateGrid();
-		this.addObjects();
+				this.frameSize = this.gridX * this.gridY * 2;
 
-		// set up event listnener for resize
-		window.addEventListener('resize', this.resize.bind(this));
+				this.frameCount = (this.displacementTextureBuffer.length - 2) / this.frameSize;
 
-		// set up mouse event listeners
-		window.addEventListener('mousedown', (e) => {
-			if (e.button == 0) {
-				this.mouseDown = true;
-			}
+				const frame = this.displacementTextureBuffer.subarray(2, 2 + this.frameSize);
+				console.log(this.gridX, this.gridY, this.frameSize, frame);
+				// use the buffer to create a DataTexture
+				this.displacementTexture = new THREE.DataTexture(
+					frame,
+					this.gridX,
+					this.gridY,
+					THREE.RGFormat,
+					THREE.FloatType
+				);
+				this.displacementTexture.magFilter = THREE.NearestFilter;
+				this.displacementTexture.needsUpdate = true;
+
+				// adjust to viewport size
+				this.resize();
+
+				// set up contents
+				this.addObjects();
+
+				// set up event listnener for resize
+				window.addEventListener('resize', this.resize.bind(this));
+			});
 		});
-		window.addEventListener('mouseup', (e) => {
-			if (e.button == 0) {
-				this.mouseDown = false;
-			}
-		});
-		window.addEventListener('mousemove', (e) => {
-			this.mouse.x = e.clientX / this.container.offsetWidth;
-			this.mouse.y = e.clientY / this.container.offsetHeight;
-
-			this.mouse.vX = this.mouseDown ? this.mouse.x - this.mouse.prevX : 0;
-			this.mouse.vY = this.mouseDown ? this.mouse.y - this.mouse.prevY : 0;
-
-			this.mouse.prevX = this.mouse.x;
-			this.mouse.prevY = this.mouse.y;
-		});
-	}
-
-	setParameters(relaxation: number, radius: number, strength: number) {
-		this.relaxation = relaxation;
-		this.radius = radius;
-		this.strength = strength;
-	}
-
-	startRecording() {
-		if (!get(timestamp).recording) {
-			console.log('starting recording');
-
-			timestamp.start();
-			this.elapsedTime.start();
-			this.frameIndex = 0;
-
-			this.playing = false;
-
-			if (this.displacementData) {
-				this.displacementData.fill(0.0);
-			}
-		}
-	}
-
-	stopRecording() {
-		if (get(timestamp).recording) {
-			console.log('stopping recording');
-
-			timestamp.stop();
-			this.elapsedTime.stop();
-		}
-	}
-
-	play() {
-		if (!get(timestamp).recording && !this.playing) {
-			console.log('playing');
-
-			this.elapsedTime.start();
-
-			this.playing = true;
-			this.frameIndex = 0;
-		}
-	}
-
-	stop() {
-		if (this.playing) {
-			console.log('stopping');
-
-			this.elapsedTime.stop();
-
-			this.playing = false;
-		}
-	}
-
-	regenerateGrid() {
-		this.gridX = Math.floor(this.container.offsetWidth / this.gridDivider);
-		this.gridY = Math.floor(this.container.offsetHeight / this.gridDivider);
-
-		// buffer for the dispacement texture
-		const size = this.gridX * this.gridY;
-		const data = new Float32Array(2 * size);
-
-		for (let i = 0; i < size; i++) {
-			// const r = Math.random();
-			// const g = Math.random();
-			const r = 0;
-			const g = 0;
-
-			const stride = i * 2;
-
-			data[stride] = r;
-			data[stride + 1] = g;
-		}
-		this.displacementData = data.slice();
-
-		// use the buffer to create a DataTexture
-		this.displacementTexture = new THREE.DataTexture(
-			data,
-			this.gridX,
-			this.gridY,
-			THREE.RGFormat,
-			THREE.FloatType
-		);
-		this.displacementTexture.magFilter = THREE.NearestFilter;
-		this.displacementTexture.needsUpdate = true;
-
-		this.displacementTexture.magFilter = this.displacementTexture.minFilter = THREE.NearestFilter;
-
-		if (this.material) {
-			this.material.uniforms.uDataTexture.value = this.displacementTexture;
-			this.material.uniforms.uDataTexture.value.needsUpdate = true;
-		}
-
-		// set up buffer for recording
-		timestamp.stop();
-
-		this.frameSize = this.displacementTexture.image.data.length;
-		this.displacementTextureBuffer = new Float32Array(
-			2 + this.frameSize * (1 / this.interval) * this.maxRecordingTime
-		);
-
-		this.resetRecording();
-	}
-
-	resetRecording() {
-		console.log('resetting');
-
-		this.stopRecording();
-		this.stop();
-
-		this.displacementTextureBuffer.fill(0.0);
-		if (this.displacementTexture) {
-			this.displacementTexture.image.data.fill(0.0);
-		}
-		if (this.displacementData) {
-			this.displacementData.fill(0.0);
-		}
-
-		this.displacementTextureBuffer[0] = this.gridX;
-		this.displacementTextureBuffer[1] = this.gridY;
-	}
-
-	getDisplacementBuffer() {
-		return this.displacementTextureBuffer;
 	}
 
 	addObjects() {
-		if (this.geometry) {
-			this.texture = new THREE.Texture(this.canvas);
-			this.texture.needsUpdate = true;
-			this.material = new THREE.ShaderMaterial({
-				extensions: { derivatives: true },
-				side: THREE.DoubleSide,
-				uniforms: {
-					uTexture: {
-						value: this.texture
-					},
-					uDataTexture: {
-						value: this.displacementTexture
-					}
+		const geometry = new THREE.PlaneGeometry(2, 2, this.gridX, this.gridY);
+
+		this.texture = new THREE.Texture(this.canvas);
+		this.texture.needsUpdate = true;
+		this.material = new THREE.ShaderMaterial({
+			extensions: { derivatives: true },
+			side: THREE.DoubleSide,
+			uniforms: {
+				uTexture: {
+					value: this.texture
 				},
-				vertexShader: vertex,
-				fragmentShader: fragment
-			});
-			this.plane = new THREE.Mesh(this.geometry, this.material);
-			this.scene.add(this.plane);
-		}
+				uDataTexture: {
+					value: this.displacementTexture
+				}
+			},
+			vertexShader: vertex,
+			fragmentShader: fragment
+		});
+		this.plane = new THREE.Mesh(geometry, this.material);
+		this.scene.add(this.plane);
 	}
 
-	updateCanavas() {
+	updateCanvas() {
 		if (this.texture && this.material) {
 			this.texture = new THREE.Texture(this.canvas);
 			this.material.uniforms.uTexture.value = this.texture;
@@ -271,80 +134,20 @@ export default class Sketch {
 		this.delta += this.clock.getDelta();
 
 		if (this.delta > this.interval) {
-			if (this.displacementTexture && this.displacementData) {
-				const data = this.displacementTexture.image.data;
-
-				if (!this.playing) {
-					for (let i = 0; i < data.length; i += 2) {
-						this.displacementData[i] = this.displacementData[i] * this.relaxation;
-						this.displacementData[i + 1] = this.displacementData[i + 1] * this.relaxation;
-					}
-
-					const gridMouseX = this.gridX * this.mouse.x;
-					const gridMouseY = this.gridY * (1 - this.mouse.y);
-					const maxDist = this.gridX * this.radius;
-
-					for (let i = 0; i < this.gridX; i++) {
-						for (let j = 0; j < this.gridY; j++) {
-							const distance = (gridMouseX - i) ** 2 / 1 + (gridMouseY - j) ** 2;
-							const maxDistSq = maxDist ** 2;
-
-							if (distance < maxDistSq) {
-								const index = 2 * (i + this.gridX * j);
-
-								let power = maxDist / Math.sqrt(distance) - 1;
-								power = clamp(power, 0, 10);
-								// if(distance <this.size/32) power = 1;
-								// power = 1;
-
-								this.displacementData[index] -= this.strength * this.mouse.vX * power;
-								this.displacementData[index + 1] += this.strength * this.mouse.vY * power;
-							}
-						}
-					}
-
-					this.mouse.vX *= 0.9;
-					this.mouse.vY *= 0.9;
-
-					// if recording, save this frame into the buffer
-					if (get(timestamp).recording) {
-						const elapsedTime = this.elapsedTime.getElapsedTime();
-						timestamp.setTime(Math.floor(elapsedTime));
-
-						if (
-							(this.frameIndex + 1) * this.frameSize <
-							this.displacementTextureBuffer.length - 2
-						) {
-							for (let i = 0; i < data.length; i++) {
-								data[i] =
-									this.displacementData[i] +
-									this.displacementTextureBuffer[i + 2 + this.frameIndex * this.frameSize];
-							}
-							this.displacementTextureBuffer.set(data, 2 + this.frameIndex * this.frameSize);
-
-							this.frameIndex++;
-						} else {
-							this.stopRecording();
-						}
-					}
-				} else {
-					// if playing, get frame from the buffer
-					timestamp.setTime(Math.floor(this.elapsedTime.getElapsedTime()));
-
-					if ((this.frameIndex + 1) * this.frameSize < this.displacementTextureBuffer.length - 2) {
-						this.displacementTexture.image.data.set(
-							this.displacementTextureBuffer.subarray(
-								2 + this.frameIndex * this.frameSize,
-								2 + (this.frameIndex + 1) * this.frameSize
-							)
-						);
-						this.frameIndex++;
-					} else {
-						this.stop();
-					}
-				}
-
+			if (this.displacementTexture && this.frameCount) {
+				// if playing, get frame from the buffer
+				this.displacementTexture.image.data.set(
+					this.displacementTextureBuffer.subarray(
+						2 + this.frameIndex * this.frameSize,
+						2 + (this.frameIndex + 1) * this.frameSize
+					)
+				);
 				this.displacementTexture.needsUpdate = true;
+
+				this.frameIndex++;
+				if (this.frameIndex >= this.frameCount) {
+					this.frameIndex = 0;
+				}
 			}
 
 			this.renderer.render(this.scene, this.camera);
@@ -354,21 +157,8 @@ export default class Sketch {
 	}
 
 	resize() {
-		this.updateCanavas();
-		this.regenerateGrid();
+		this.updateCanvas();
 
-		const width = this.container.offsetWidth;
-		const height = this.container.offsetHeight;
-		if (this.geometry) {
-			this.geometry.dispose();
-		}
-		this.geometry = new THREE.PlaneGeometry(
-			2,
-			2,
-			Math.floor(width / this.gridDivider),
-			Math.floor(height / this.gridDivider)
-		);
-
-		this.renderer.setSize(width, height);
+		this.renderer.setSize(this.container.offsetWidth, this.container.offsetHeight);
 	}
 }
